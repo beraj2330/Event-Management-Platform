32017239072150d9a3c7969eced1348304409afe
Jeel Bera <jeelbera2330@gmail.com>
2024-11-26 19:41:05 +0530

    removed app service and controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -20,7 +18,5 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     UserModule,
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
