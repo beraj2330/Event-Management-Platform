9440a70a8048f8c04f9d643bb6b2c0983fcf96b1
Jeel Bera <jeelbera2330@gmail.com>
2024-11-26 16:57:19 +0530

    added auth service and jwt-payload interface

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/user/user.entity';
+import { UserService } from 'src/user/user.service';
+import { Repository } from 'typeorm';
+import { bcrypt} from 'bcryptjs';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(
+        private jwtService: JwtService,
+        @InjectRepository(User)
+        private userRepository: Repository<User>,
+        private userService: UserService,
+    ) {}
+
+    async login(email: string, password: string): Promise<any> {
+        const user = await this.userService.findByEmail(email);
+        if(!user) {
+            throw new Error('User not found');
+        }
+
+        const passwordMatches = await bcrypt.compare(password, user.password);
+        if(!passwordMatches) {
+            throw new Error('Invalid Password');
+        }
+
+        const payload: JwtPayload = { email: user.email, sub: user.id };
+        const token = this.jwtService.sign(payload);
+
+        return {
+            token,
+            user: {email: user.email, id: user.id},
+        };
+    }
+}
---
+++ backend/src/auth/jwt-payload.interface.ts
@@ -0,0 +1,5 @@
+export interface JwtPayload {
+    sub: number; // Typically, this would be the user ID
+    email: string; // The user's email
+  }
+  
