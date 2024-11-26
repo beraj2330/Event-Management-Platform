7c882049eb1723d46b5ed361aa0fe3ab9d4b51ad
Jeel Bera <jeelbera2330@gmail.com>
2024-11-26 15:11:15 +0530

    updated the service logic for the user

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import {InjectRepository} from '@nestjs/typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    constructor(
+        @InjectRepository(User)
+        private userRepository: Repository<User>
+    ) {}
+
+    async findByEmail(email:string): Promise<User | undefined> {
+        return this.userRepository.findOne({where: {email}});
+    }
+
+    async createUser(createUserDto: CreateUserDto): Promise<User> {
+        const user = this.userRepository.create(createUserDto);
+        return this.userRepository.save(user);
+    }
+}
